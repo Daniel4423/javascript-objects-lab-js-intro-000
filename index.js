98387f9ead626982586329239c69e318eccb4d3f
Daniel Bailey <daniel4423@gmail.com>
2019-01-31 16:24:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
 
-destructivelyUpdateWithKeyAndValue(object, key, value) {
+function destructivelyUpdateWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
